1888b7cf196cb7d653b3c8a706b3e384c3baa902
Maeli <maeli.palharini@hotmail.com>
2025-09-01 20:18:15 -0300

    🔧 fix: configurar base do Vite para GH Pages e simplificar tratamento de erros

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <AuthProvider>
-            <App />
+            <HashRouter>
+                <App />
+            </HashRouter>
         </AuthProvider>
     </React.StrictMode>
 );
