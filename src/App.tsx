d66e69a16aeb9b8a504ac862f034563315263cf3
Maeli <maeli.palharini@hotmail.com>
2025-09-02 21:37:37 -0300

    🚀 feat(routes): incluir rotas do tema no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
-import {AuthProvider} from "./contexts/AuthContext.tsx";
+import {AuthProvider} from "./contexts/AuthContext.tsx"
+import ListaTemas from "./components/tema/listatemas/ListaTemas.tsx";
+import FormTema from "./components/tema/formtema/FormTema.tsx";
+import DeletarTema from "./components/tema/deletartema/DeletarTema.tsx";
 
 export default function App() {
     return (
@@ -14,13 +17,16 @@ export default function App() {
                 <BrowserRouter>
                     <div className="min-h-screen flex flex-col">
                         <Navbar />
-                        <main className="flex-1">
+                        <main className="flex-1 bg-yellow-50">
                             <Routes>
                                 <Route path="/" element={<Navigate to="/login" replace />} />
                                 <Route path="/login" element={<Login />} />
                                 <Route path="/home" element={<Home />} />
                                 <Route path="/cadastro" element={<Cadastro />} />
-                                <Route path="*" element={<Navigate to="/login" replace />} />
+                                <Route path="/temas" element={<ListaTemas />} />
+                                <Route path="/cadastrartema" element={<FormTema />} />
+                                <Route path="/editartema/:id" element={<FormTema />} />
+                                <Route path="/deletartema/:id" element={<DeletarTema />} />
                             </Routes>
                         </main>
                         <Footer />
